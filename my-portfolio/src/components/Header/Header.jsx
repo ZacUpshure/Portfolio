c29729c187bcb16fe5a320d17eb53df6c3d0aac8
Zac <zac.upshure@outlook.com>
2023-05-09 00:52:00 +0200

    test

--- my-portfolio/src/components/Header/Header.jsx
+++ my-portfolio/src/components/Header/Header.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
-
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
+        <a>
           <DiCssdeck size="3rem" /> <span>Portfolio</span>
         </a>
       </Link>
